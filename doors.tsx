476b33cb9cdfedf4d32f299672c427b160f68b3d
Marcel Eberl <marcel-eberl@gmx.at>
2019-02-12 07:31:03 +0100

    MapGrob fertig

---
+++ doors.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="Doors" tilewidth="48" tileheight="52" tilecount="80" columns="8">
+ <image source="tiledSet/door.png" width="384" height="544"/>
+</tileset>
