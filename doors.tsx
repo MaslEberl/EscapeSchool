f65981557d8c1261ad85bc0f419b40419cb5e660
Marcel Eberl <marcel-eberl@gmx.at>
2019-02-25 08:17:17 +0100

    250219 Map Level1 Fertig

--- doors.tsx
+++ doors.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.2" name="Doors" tilewidth="48" tileheight="52" tilecount="80" columns="8">
+<tileset version="1.2" tiledversion="1.2.2" name="DoorsNew" tilewidth="32" tileheight="48" tilecount="132" columns="12">
  <image source="tiledSet/door.png" width="384" height="544"/>
 </tileset>
